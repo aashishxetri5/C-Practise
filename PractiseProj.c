5ed91089781ba74206371d83c8ed09db723bdef1
Aashish Katwal <43494041+aashishxetri5@users.noreply.github.com>
2021-04-12 22:37:24 +0545

    Update PractiseProj.c

--- PractiseProj.c
+++ PractiseProj.c
@@ -2,14 +2,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int main()
-{
-
+int main(){
+    bool shouldContinue = true;
+    int userPreference;
+    
     printf("\nHey user, welcome\n");
     printf("Please select which of the following action you want to perform...\n\n");
 
-    bool shouldContinue = true;
-    int userPreference;
 showOptions:
     printf("Press '1' to calculate Simple Interest...\n");
     printf("Press '2' to find smallest number among three...\n");
@@ -17,12 +16,11 @@ showOptions:
     printf("Press '4' to find if you're elligible to vote...\n");
     printf("Press '5' to print table of any number...\n");
     scanf("%d", &userPreference);
-    // system("cls");
-    while (shouldContinue)
-    {
+    
+    /*loops unless the user chooses to perform no more actions later in the program*/
+    while (shouldContinue){
 
-        switch (userPreference)
-        {
+        switch (userPreference){
             int num1, num2, num3, age, number, principal;
             float rate, time;
 
@@ -40,42 +38,36 @@ showOptions:
 
             break;
 
-        /*Finds Smallest Number*/
+        /*Prints the Smallest Number among three numbers given by User*/
         case 2:
 
             printf("Enter three numbers...\n");
             scanf("%d %d %d", &num1, &num2, &num3);
 
-            if (num1 < num2 && num1 < num3)
-            {
+            if (num1 < num2 && num1 < num3){
                 printf("%d is smaller than %d and %d", num1, num2, num3);
             }
-            else if (num2 < num1 && num2 < num3)
-            {
+            else if (num2 < num1 && num2 < num3){
                 printf("%d is smaller than %d and %d", num2, num1, num3);
             }
-            else if (num3 < num2 && num3 < num1)
-            {
+            else if (num3 < num2 && num3 < num1){
                 printf("%d is smaller than %d and %d", num3, num1, num2);
             }
             break;
 
-        /*Find Greatest Number*/
+        /*Prints the Greatest Number among three numbers given by User*/
         case 3:
 
             printf("Enter three numbers...\n");
             scanf("%d %d %d", &num1, &num2, &num3);
             
-            if (num1 > num2 && num1 > num3)
-            {
+            if (num1 > num2 && num1 > num3){
                 printf("%d is greater than %d and %d", num1, num2, num3);
             }
-            else if (num2 > num1 && num2 > num3)
-            {
+            else if (num2 > num1 && num2 > num3){
                 printf("%d is greater than %d and %d", num2, num1, num3);
             }
-            else if (num3 > num1 && num3 > num2)
-            {
+            else if (num3 > num1 && num3 > num2){
                 printf("%d is greater than %d and %d", num3, num1, num2);
             }
             break;
@@ -85,12 +77,11 @@ showOptions:
 
             printf("Enter your age... ");
             scanf("%d", &age);
-            if (age >= 18)
-            {
+                
+            if (age >= 18){
                 printf("You are elligible to vote.");
             }
-            else
-            {
+            else{
                 printf("You are not elligible to vote");
             }
             break;
@@ -101,8 +92,7 @@ showOptions:
             printf("Enter the number you want to print the table of...\n");
             scanf("%d", &number);
 
-            for (int i = 1; i <= 10; i++)
-            {
+            for (int i = 1; i <= 10; i++){
                 printf("%d * %d = %d\n", number, i, number * i);
             }
             break;
@@ -112,16 +102,13 @@ showOptions:
         }
 
         int continuity;
-        printf("\n\nWould you like to continue?(1/0)...\n");
+        printf("\n\nWould you like to continue?(1/0)...\n"); // 1 = Yes and 0 = true
         scanf("%d", &continuity);
 
-        if (continuity == 1)
-        {
+        if (continuity == 1){
             system("cls");
             goto showOptions;
-        }
-        else
-        {
+        }else{
             shouldContinue = false;
             printf("Thank you for using this application. Have a Good Day!!");
         }
